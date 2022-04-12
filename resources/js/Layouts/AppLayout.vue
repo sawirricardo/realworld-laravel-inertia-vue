e53544cf7c6554b3b26b48d2d87af42e6d340f34
sawirricardo <sawir.ricardo@gmail.com>
2022-04-12 22:05:02 +0700

    Modified

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { Head, Link } from "@inertiajs/inertia-vue3";
+</script>
+<template>
+    <div>
+        <Head :title="title" />
+        <header></header>
+        <main><slot /></main>
+    </div>
+</template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -4,13 +4,18 @@ import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/inertia-vue3";
 import { InertiaProgress } from "@inertiajs/progress";
 
+const appName =
+    window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
+
 createInertiaApp({
-    resolve: (name) => require(`./Pages/${name}`),
-    setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => require(`./Pages/${name}.vue`),
+    setup({ el, app, props, plugin }) {
+        return createApp({ render: () => h(app, props) })
             .use(plugin)
+            .mixin({ methods: { route } })
             .mount(el);
     },
 });
 
-InertiaProgress.init();
+InertiaProgress.init({ color: "#4B5563" });
